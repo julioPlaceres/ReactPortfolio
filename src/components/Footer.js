88a5156cc58359ba7b18161c3aee4d52c0528cba
dreamheart <leondreamheart@outlook.com>
2021-08-27 15:27:06 -0400

    Footer Completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,46 @@
+// Import required Files
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   return (
-    <Container >
+    <Container fluid className="footer">
+      <Row>
+        {/* Project Name */}
+        <Col md="4" className="footer2">
+          <h3>Julio A Placeres</h3>
+        </Col>
+        {/* Social Icons */}
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/julioPlaceres"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/julio-placeres-731280a7/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
     </Container>
   );
 }
 
+// Export Module
 export default Footer;
