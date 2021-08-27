f917b7ccd802984a093606af4c9a803c2877d541
dreamheart <leondreamheart@outlook.com>
2021-08-27 18:43:58 -0400

    Resume completed

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,9 +1,64 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Resumecontent from "./ResumeContent";
+import pdf from "../../Assets/Placeres_Resume.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
 
 function Resume() {
   return (
-    <Container> </Container>
+    <Container fluid className="resume-section-main">
+      <Container>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button variant="primary" href={pdf} target="_blank">
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+        <Row className="resume">
+          <Col md={6} className="resume-left">
+            <h3 className="resume-title">Experience</h3>
+            <Resumecontent
+              title="Computer Programmer"
+              date="January 2017 - Present"
+              content={[
+                "Developed tracking systems to record issues on the manufacturing floor." +
+                " in combination with dashboards to provide management with the necessary information to reduce cost and waste." +
+                "Also, Developed multiple WPF applications for auditing and other production processes while." +
+                " mantaining multiple SQL Databases.",
+              ]}
+            />
+            <Resumecontent
+              title="Quality Auditor and Metrologist"
+              content={[
+                "Created and maintained programs for molding parts using CNC Sprint (Vision System). Inspected a large" +
+                "variety of components, charted and created reports using SPC. Performed FAI for external customers and part approvals." +
+                "Performed line and finished goods audits and ISO audits. Reporting to management and creating" +
+                "corrective/preventive actions when needed. Issuing CAPAs forms",
+              ]}
+            />
+          </Col>
+          <Col md={6} className="resume-right">
+            <h3 className="resume-title">Education</h3>
+            <Resumecontent
+              title="Process Control Engineering [Havana, Cuba]"
+              date="September 2008 - June 2012"
+              content={["Process control Engineering Major"]}
+            />
+            <Resumecontent
+              title="UCF [Orlando, Florida]"
+              date="June 14, 2021 - September 07, 2021"
+              content={["Full Stack Web development"]}
+            />
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button variant="primary" href={pdf} target="_blank">
+          <AiOutlineDownload />&nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </Container>
   );
 }
 
--- src/components/Resume/ResumeContent.js
+++ src/components/Resume/ResumeContent.js
@@ -1,9 +1,22 @@
+// Import Require Files
 import React from "react";
 
 function ResumeContent(props) {
   return (
-    <div> </div>
+    <div className="resume-item">
+      <h5 className={props.title ? "resume-title" : "resume-no-title"}>
+        {props.title}
+      </h5>
+      <p>
+        <em>{props.date}</em>
+      </p>
+      <ul>
+        {props.content.map((value, index) => (
+          <li key={index}> ‣ {value}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
-
+// Export Module
 export default ResumeContent;
